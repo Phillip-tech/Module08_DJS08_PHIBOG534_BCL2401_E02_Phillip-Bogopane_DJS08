9e2e306be0318926e36280d79ee39145f56e2482
Phillip Bogopane <pm.bogopane@gamil.com>
2024-06-06 15:10:15 +0200

    Coded browser router code.

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {BrowserRouter,Routes } from "react-router-dom"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  /**
+   * Challenge: set up the BrowserRouter and Routes!
+   * Note: nothing will render to the page yet.
+   */
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <BrowserRouter>
+      <Routes>
+        
+      </Routes>
+    </BrowserRouter>
+  );
