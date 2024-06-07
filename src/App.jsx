914890b9153789e3b3f3499b600c519c00a67487
Phillip Bogopane <pm.bogopane@gamil.com>
2024-06-07 09:53:35 +0200

    Restored code in main and typed export default before function App.

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,10 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Vans from "./pages/Vans"
-
 import "./server"
 
 
-function App() {
+export default function App() {
   return (
     <BrowserRouter>
       <header>
